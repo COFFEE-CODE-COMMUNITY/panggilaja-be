3f864bf4c2853cc56e3d2c5d3472324fe8674b13
RyanHandhika <muhammadryanhandhikasusantorya@gmail.com>
2025-10-07 15:17:18 +0700

    prepare all library

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
-import dotenv from "dotenv";
-dotenv.config();
+import createApp from "./app.js";
+import config from "./config/index.js";
 
-import express from "express";
-const app = express();
+const app = createApp();
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`app listening on port http://localhost/${PORT}`);
+const server = app.listen(config.port, () => {
+  console.info(`Server running on port : ${config.port}`);
+  console.info(`Environment: ${config.env}`);
+});
+
+server.on("error", (err) => {
+  console.info(`Failed to start server: ${err}`);
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("💥 Uncaught Exception:", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("💥 Unhandled Rejection at:", promise, "reason:", reason);
+  process.exit(1);
 });
